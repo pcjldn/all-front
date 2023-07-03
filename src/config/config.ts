405efde76256e9bf9ba0223481ebba14c0eb0c9b
pcj <1808957746@qq.com>
2023-07-03 09:14:51 +0800

    工作待办

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,8 @@
 export default {
 
   billHost:'http://101.132.187.55:8088',
-  workHost:'http://101.132.187.55:8088',
+  // workHost:'http://101.132.187.55:8088',
+  workHost:'http://127.0.0.1:8088',
 
   maxLodingTime: 1000 * 60,
 }
--- src/views/work/todo/ToDoList.vue
+++ src/views/work/todo/ToDoList.vue
@@ -5,7 +5,63 @@
     <!--主区域-->
     <div class="main-box">
       <!--搜索区-->
-      <div class="search-box"></div>
+      <div class="search-box">
+        <el-form
+            label-width="100px"
+            :model="searchData"
+            label-suffix=":"
+        >
+          <el-row gutter="40">
+            <el-col :span="5">
+              <el-form-item label="待办类型">
+                <el-select v-model="searchData.todoType" class="m-2" placeholder="请选择" style="width: 100%">
+                  <el-option
+                      v-for="item in todoTypeOptions"
+                      :key="item.id"
+                      :label="item.typeName"
+                      :value="item.id"
+                  />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="5">
+              <el-form-item label="状态">
+                <el-select v-model="searchData.status" class="m-2" placeholder="请选择" style="width: 100%">
+                  <el-option
+                      v-for="item in statusOptions"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.value"
+                  />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="5">
+              <el-form-item label="所属用户">
+                <el-select v-model="searchData.todoUserId" class="m-2" placeholder="请选择" style="width: 100%">
+                  <el-option
+                      v-for="item in userOptions"
+                      :key="item.id"
+                      :label="item.userName"
+                      :value="item.id"
+                  />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="5">
+              <el-form-item label="待办内容">
+                <el-input type="text" v-model="searchData.todoContent"/>
+              </el-form-item>
+            </el-col>
+            <el-col :span="4">
+              <el-button type="primary" @click="getTodo">查询</el-button>
+              <el-button type="warning" @click="clearSearchAndQuery">重置</el-button>
+            </el-col>
+          </el-row>
+        </el-form>
+
+
+      </div>
       <!--顶部按钮-->
       <div class="top-action-box">
         <el-button type="primary" @click="add">新增</el-button>
@@ -17,23 +73,22 @@
           <el-table-column prop="todoContent" label="待办内容" width="500"/>
           <el-table-column prop="statusName" label="待办状态">
             <template #default="{ row }">
-        <span :class="row.status === 1? 'waitToDo' :'readyDo'">
-          {{ row.statusName }}
-        </span>
+              <span :class="row.status === 1? 'waitToDo' :'readyDo'">
+                {{ row.statusName }}
+              </span>
             </template>
           </el-table-column>
-          <el-table-column prop="todoUserId" label="所属用户"/>
+          <el-table-column prop="todoUserName" label="所属用户"/>
           <el-table-column prop="createTime" label="创建时间">
             <template #default="{ row }">
               {{ formatDate(row.createTime) }}
             </template>
           </el-table-column>
           <el-table-column fixed="right" label="操作" width="200">
-            <template #default>
-              <el-button link type="primary" size="small">明细</el-button>
-              <el-button link type="primary" size="small">编辑</el-button>
-              <el-button link type="primary" size="small">已同步</el-button>
-              <el-button link type="primary" size="small">删除</el-button>
+            <template #default="{ row }">
+              <el-button link type="primary" size="small" @click="editForm(row)">编辑</el-button>
+              <el-button link type="primary" size="small" @click="readyDo(row)">已同步</el-button>
+              <el-button link type="primary" size="small" @click="delData(row)">删除</el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -42,7 +97,7 @@
 
 
     <!--抽屉-->
-    <el-drawer v-model="drawer" :direction="direction" size="50%">
+    <el-drawer v-model="drawer" :direction="direction" size="50%" :before-close="closeDrawer">
       <template #header>
         <h4>{{ formTitle }}</h4>
       </template>
@@ -58,9 +113,9 @@
               <el-select v-model="formData.todoType" class="m-2" placeholder="请选择" style="width: 100%">
                 <el-option
                     v-for="item in todoTypeOptions"
-                    :key="item.value"
-                    :label="item.label"
-                    :value="item.value"
+                    :key="item.id"
+                    :label="item.typeName"
+                    :value="item.id"
                 />
               </el-select>
             </el-form-item>
@@ -82,9 +137,9 @@
               <el-select v-model="formData.todoUserId" class="m-2" placeholder="请选择" style="width: 100%">
                 <el-option
                     v-for="item in userOptions"
-                    :key="item.value"
-                    :label="item.label"
-                    :value="item.value"
+                    :key="item.id"
+                    :label="item.userName"
+                    :value="item.id"
                 />
               </el-select>
             </el-form-item>
@@ -111,6 +166,8 @@
 import {ref, onMounted} from 'vue';
 import request from "@/utils/request.ts";
 import config from "@/config/config.ts";
+import {ElMessage} from "element-plus";
+import $ from 'jquery'
 
 const tableData = ref([])
 const drawer = ref(false)
@@ -118,26 +175,31 @@ const direction = ref('rtl')
 const formTitle = ref('待办详情')
 const searchData = ref({});
 const formData = ref({});
+
 const statusOptions = ref([
   {
     label: "未同步",
-    value: "1"
+    value: 1
   },
   {
     label: "已同步",
-    value: "2"
+    value: 2
   }
 ]);
+const userOptions = ref([]);
+const todoTypeOptions = ref([]);
 
 
 const mount = () => {
   getTodo();
+  getUserCheck();
+  getTodoTypeOptions();
 };
 
 function getTodo() {
   request({
     url: config.workHost + "/workTodo/queryByInfo",
-    data: searchData
+    data: searchData.value
   }).then(res => {
     tableData.value = res.data;
   })
@@ -160,22 +222,127 @@ const formatDate = (timestamp) => {
 
 function add() {
   drawer.value = true;
+  formData.value.status = 1;
 }
 
+function editForm(row) {
+  drawer.value = true;
+  formData.value = $.extend({}, row);
+}
 
+function clearSearchAndQuery() {
+  for (let valueKey in searchData.value) {
+    searchData.value[valueKey] = ''
+  }
+  getTodo()
+}
 
+function readyDo(row) {
+  row.status = 2;
+  request({
+    url: config.workHost + "/workTodo/insertOrUpdate",
+    data: row
+  })
+      .then(res => {
+        if (res.code === 200) {
+          ElMessage({
+            message: "操作成功",
+            type: "success"
+          })
+          drawer.value = false;
 
+          getTodo();
+          clearFormData()
+        } else {
+          ElMessage.error("操作失败");
+        }
+      })
+      .catch(() => {
+        ElMessage.error("操作异常");
+      })
+}
 
+function delData(row) {
+  request({
+    url: config.workHost + "/workTodo/del",
+    data: row
+  })
+      .then(res => {
+        if (res.code === 200) {
+          ElMessage({
+            message: "删除成功",
+            type: "success"
+          })
+
+          getTodo();
+        } else {
+          ElMessage.error("删除失败");
+        }
+      })
+      .catch(() => {
+        ElMessage.error("删除异常");
+      })
+}
+
+const closeDrawer = (done) => {
+  clearFormData();
+  done();
+}
 
 // ------------------表单方法
-// function se
+function getUserCheck() {
+  request(config.workHost + "/ucUser/queryByInfo")
+      .then(res => {
+        userOptions.value = res.data;
+      })
+      .catch(() => {
+        ElMessage.error("获取人员信息出错了");
+      })
+}
+
+function getTodoTypeOptions() {
+  request(config.workHost + "/workTodoType/queryByInfo")
+      .then(res => {
+        todoTypeOptions.value = res.data;
+      })
+      .catch(() => {
+        ElMessage.error("获取待办类型出错了");
+      })
+}
 
 function cancelClick() {
   drawer.value = false;
+  clearFormData()
 }
 
 function confirmClick() {
+  request({
+    url: config.workHost + "/workTodo/insertOrUpdate",
+    data: formData.value
+  })
+      .then(res => {
+        if (res.code === 200) {
+          ElMessage({
+            message: "操作成功",
+            type: "success"
+          })
+          drawer.value = false;
 
+          getTodo();
+          clearFormData()
+        } else {
+          ElMessage.error("保存失败");
+        }
+      })
+      .catch(() => {
+        ElMessage.error("保存失败");
+      })
+}
+
+function clearFormData() {
+  for (let valueKey in formData.value) {
+    formData.value[valueKey] = '';
+  }
 }
 </script>
 
