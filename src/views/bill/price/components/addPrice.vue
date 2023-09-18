d72472303d91b421a7b9383c6a77fb23a484ff5a
pcj <1808957746@qq.com>
2023-09-18 23:00:44 +0800

    部分待办代码

---
+++ src/views/bill/price/components/addPrice.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="main-form">
+    <el-form
+        label-position="right"
+        label-width="100px"
+        :model="formData"
+        style="max-width: 460px"
+    >
+      <el-form-item label="商品名称">
+        <el-input v-model="formData.name"/>
+      </el-form-item>
+      <el-form-item label="价格">
+        <el-input v-model="formData.price"/>
+      </el-form-item>
+      <el-form-item label="类型">
+<!--        <el-input v-model="formData.typeId"/>-->
+        <el-select v-model="formData.typeId" style="width: 100%" class="m-2" placeholder="请选择">
+          <el-option
+              v-for="item in typeOptions"
+              :key="item.id"
+              :label="item.typeName"
+              :value="item.id"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="地址">
+        <el-input v-model="formData.place"/>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="sureAdd">
+          {{ action == 'add' ? '添加' :'确定' }}
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import request from "@/utils/request.ts";
+import config from "@/config/config.ts";
+import {ElMessage} from "element-plus";
+
+export default {
+  name: "addPrice",
+  props: {
+    drawer: {
+      type: Boolean,
+      default: () => false
+    }
+  },
+  data() {
+    return {
+      formData: {
+        name: '',
+        price: '',
+        typeId: '',
+        place: ''
+      },
+      action: 'add',
+      typeOptions: []
+    }
+  },
+  mounted() {
+    this.getTypeInfo()
+  },
+  methods: {
+    getTypeInfo() {
+      request.get(
+          config.billHost + "/extra/goodsType/do")
+          .then(res => {
+            this.typeOptions = res.data
+            this.formData.typeId = this.typeOptions[0].id
+          })
+    },
+    sureAdd() {
+      if(this.action == 'add'){
+        this.add()
+      }else if(this.action == 'edit'){
+        this.edit()
+      }
+    },
+    add(){
+      request.post(
+          config.billHost + "/extra/goodsPrice/do",this.formData)
+          .then(res => {
+            if(res.code == 200){
+              ElMessage({
+                message: "添加成功",
+                type: "success"
+              })
+            }
+          })
+    },
+    edit(){
+      request.put(
+          config.billHost + "/extra/goodsType/do",this.formData)
+          .then(res => {
+            if(res.code == 200){
+              ElMessage({
+                message: "添加成功",
+                type: "success"
+              })
+            }
+          })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.main-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+::v-deep .el-form {
+  width: 100%;
+}
+</style>
--- src/views/bill/price/index.vue
+++ src/views/bill/price/index.vue
@@ -1,109 +1,130 @@
 <template>
-<div>
-  <div class="search-box">
-    <el-form :inline="true" :model="searchData" class="search-form">
-      <el-row>
-        <el-col :span="9">
-          <el-form-item label="">
-            <el-input style="width: 100%" v-model="searchData.goodName" placeholder="商品名" clearable />
-          </el-form-item>
-        </el-col>
-        <el-col :span="9">
-          <el-form-item label="">
-            <el-select style="width: 100%"
-                v-model="searchData.goodtype"
-                placeholder="商品类型"
-                clearable
-            >
-              <el-option v-for="item in goodTypeOption" :label="item.typeName" :value="item.id" :key="item.id "/>
-            </el-select>
-          </el-form-item>
-        </el-col>
-        <el-col :span="4">
-          <el-form-item label="">
-            <el-button type="primary" size="default" @click="search()">查询</el-button>
-<!--            <el-button>Cancel</el-button>-->
-          </el-form-item>
-        </el-col>
-      </el-row>
-    </el-form>
-  </div>
-  <div class="main-box">
-    <el-table :data="tableData" height="100%" style="width: 100%">
-      <el-table-column prop="name" label="名称"  />
-      <el-table-column prop="minPrice" label="价格"  >
-        <template #default="scope">
-        <span>{{scope.row.price}}(
-          <span style="color: #0077aa;">{{scope.row.minPrice}}</span>
+  <div>
+    <div class="search-box">
+      <el-form :inline="true" :model="searchData" class="search-form">
+        <el-row>
+          <el-col :span="9">
+            <el-form-item label="">
+              <el-input style="width: 100%" v-model="searchData.goodName" placeholder="商品名" clearable/>
+            </el-form-item>
+          </el-col>
+          <el-col :span="9">
+            <el-form-item label="">
+              <el-select style="width: 100%" @change="search()"
+                         v-model="searchData.goodtype"
+                         placeholder="商品类型"
+                         clearable
+              >
+                <el-option v-for="item in goodTypeOption" :label="item.typeName" :value="item.id" :key="item.id "/>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="4">
+            <el-form-item label="">
+              <el-button type="primary" size="default" @click="search()">查询</el-button>
+              <!--            <el-button>Cancel</el-button>-->
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+    </div>
+    <div class="main-box">
+      <el-table :data="tableData" height="100%" style="width: 100%">
+        <el-table-column prop="name" label="名称" min-width="70"/>
+        <el-table-column prop="minPrice" label="价格" min-width="100">
+          <template #default="scope">
+        <span>{{ scope.row.price }}(
+          <span style="color: #0077aa;">{{ scope.row.minPrice }}</span>
           /
-          <span style="color: red;"> {{scope.row.maxPrice}}</span>)</span>
-        </template>
-      </el-table-column>
-      <el-table-column prop="typeName" label="类型" />
-      <el-table-column prop="action" label="操作" width="200">
-        <template #default="scope">
-          <el-button-group>
-            <el-button type="primary" size="small" @click="addNew()">编辑</el-button>
-            <el-button type="warning" size="small" @click="edit()">查看</el-button>
-            <el-button type="danger" size="small" @click="del()">删除</el-button>
-          </el-button-group>
+          <span style="color: red;"> {{ scope.row.maxPrice }}</span>)</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="typeName" label="类型" min-width="55"/>
+        <el-table-column prop="action" label="操作" min-width="165">
+          <template #default="{row}">
+            <el-button-group>
+              <el-button type="primary" size="small" @click="edit(row)">编辑</el-button>
+              <el-button type="warning" size="small" @click="view()">查看</el-button>
+              <el-button type="danger" size="small" @click="del()">删除</el-button>
+            </el-button-group>
 
-        </template>
-      </el-table-column>
-    </el-table>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+
+
+    <el-drawer
+        v-model="addDialog"
+        :title="dialogTitle"
+        size="80%"
+        :direction="addDialogPostion"
+    >
+      <add-price :drawer="addDialog" ref="dialog"></add-price>
+    </el-drawer>
+
   </div>
-</div>
 </template>
 
 <script>
 import request from "@/utils/request.ts";
 import config from "@/config/config.ts";
+import AddPrice from "@/views/bill/price/components/addPrice.vue";
 
 export default {
   name: "index",
-  data(){
-    return{
-      tableData:[],
+  components: {AddPrice},
+  data() {
+    return {
+      tableData: [],
       searchData: {
         goodName: '',
         goodtype: ''
       },
       goodTypeOption: [],
+      addDialog: true,
+      addDialogPostion: 'btt',
+      dialogTitle: '新增商品'
     }
   },
   mounted() {
     this.init();
   },
-  methods:{
-    init(){
+  methods: {
+    init() {
       this.search()
       this.getGoodTypeOption()
     },
-    search(){
+    search() {
       request.post(
-        config.billHost + "/extra/goodsPrice/devide",
-        {
-          "nameOrId":this.searchData.goodName,
-          "typeId": this.searchData.goodtype
-        }
-    )
-          .then(res=>{
+          config.billHost + "/extra/goodsPrice/devide",
+          {
+            "nameOrId": this.searchData.goodName,
+            "typeId": this.searchData.goodtype
+          }
+      )
+          .then(res => {
             this.tableData = res.data
           })
     },
-    getGoodTypeOption(){
+    getGoodTypeOption() {
       request.get(config.billHost + "/extra/goodsType/do")
-          .then(res=>{
-            console.log(res)
+          .then(res => {
             this.goodTypeOption = res.data
           })
+    },
+    edit(row) {
+      this.dialogTitle = '编辑商品信息'
+      this.addDialog = true
+      this.$refs.dialog.formData = row
+      this.$refs.dialog.action = 'edit'
     }
   }
 }
 </script>
 
 <style scoped>
-.search-box{
+.search-box {
   width: 100%;
   height: 50px;
   line-height: 50px;
@@ -111,9 +132,25 @@ export default {
   padding: 10px;
   background: #2c3e50;
 }
-.main-box{
+
+.main-box {
   width: 100%;
   height: calc(100vh - 70px - 60px);
   overflow: scroll;
+}
+
+::v-deep .el-table__cell {
+  padding: 5px 0;
+}
+
+/*::v-deep .el-drawer__body{*/
+/*  padding: 10px;*/
+/*}*/
+
+::v-deep .el-drawer__header {
+  padding: 10px;
+  margin-bottom: 0;
+  background: #0077aa;
+  color: #ffffff;
 }
 </style>
